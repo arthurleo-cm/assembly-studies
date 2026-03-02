5d47d94f1cc534484571bfbc1c7ec92a0e4faddb
arthurleo-cm <arthurhleocadio@gmail.com>
2026-03-01 23:23:48 -0300

    Add files via upload
    IMPLEMENTE, EM ASSEMBLER, UMA APLICAÇÃO
    DE SOFTWARE PARA CALCULAR A MÉDIA
    ARITMÉTICA DE 5 NÚMEROS:

---
+++ Fundamentos/MEDIAA12.asm
@@ -0,0 +1,37 @@
+;BASE
+.model small
+.stack 100h
+
+.data
+
+
+.code
+main:
+    mov ax, @data
+    mov ds, ax
+    
+    mov ax,9
+    mov bx,10
+    add ax,bx
+    
+    mov bx,8
+    add ax,bx
+    
+    mov cx,4
+    add ax,cx
+    
+    mov cx,10
+    add ax,cx
+    
+    mov dx,0
+    mov bx,5
+    div bx
+    
+      
+    
+sairPrograma:
+ ; Finaliza o programa
+ mov ah, 4Ch  ; Funcao de saida
+ int 21h      ; Chama a interrupcao para sair  
+
+end main   
