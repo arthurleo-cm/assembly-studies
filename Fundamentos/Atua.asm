0ca3f0c4f0350cc5c882c4a1ae8b4a1d66238429
Arthur <arthurhleocadio@gmail.com>
2026-04-19 13:53:23 -0300

    Update Atua.asm

--- Fundamentos/Atua.asm
+++ Fundamentos/Atua.asm
@@ -13,20 +13,31 @@ scanCode db 0
 caracterRepeticao dw 1
 dlin db 1
 dcol db 1
+raqueteCol db 2 
+raqueteLin db 2
+raqueteTam dw 18
+caracterCor db 28h
 
 .code
+
+
 main:
     mov ax, @data
     mov ds, ax
+    
+    
      mov cursorLin,5
     mov cursorCol,10
+    
+    mov raqueteLin,18
+    mov raqueteCol,20
     
     mov dcol,1
     mov dlin,1   
    
     
 loop:
-
+    
     
       ;POSICIONAR O CURSO
       mov bh,paginaVideo
@@ -40,7 +51,70 @@ loop:
      mov cx,caracterRepeticao
      mov ah,0Ah  
      int 10h 
+     
+     
+     ; COLOCANDO A RAQUETE
+     mov ah,02h
+    mov bh,paginaVideo
+    mov dh,raqueteLin
+    mov dl,raqueteCol
+    int 10h
+    
+    mov al,"_"
+     mov bh,paginaVideo
+     mov cx,raqueteTam
+     mov bl,31h
+     mov ah,0Ah  
+     int 10h 
+     
+     mov ah,10h
+     int 16h
+     mov tecla,al
+     mov scanCode,ah
+     
+     ;RAQUETE E CONTROLE
+     ;apagar a raquete
+     
+    mov ah,02h
+    mov bh,paginaVideo
+    mov dh,raqueteLin
+    mov dl,raqueteCol
+    int 10h
+    
+    mov al,""
+     mov bh,paginaVideo
+     mov cx,raqueteTam
+     mov bl,0h
+     mov ah,0Ah  
+     int 10h 
+    
+    
+     
+     cmp tecla,","
+     jnz desvioraquete001
+     cmp raqueteCol,0
+     je  desvioraquete001
+     dec raqueteCol
+     
+     
 
+     
+ desvioraquete001:
+     cmp tecla,"."
+     jnz desvioraquete002
+     cmp raqueteCol,60
+     je desvioraquete002 
+     inc raqueteCol
+     
+     
+ desvioraquete002:    
+     
+     
+     
+     
+     
+     
+     
     mov ah,10h 
     int 16h
     mov tecla,al
